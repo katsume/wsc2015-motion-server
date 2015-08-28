445a90851676128338ceb960168ca13593aadd08
Yuichiro Katsume <y@katsu.me>
2015-08-29 00:22:19 +0900

    Modify

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ var app= require('express')(),
 	http= require('http').Server(app),
 	io= require('socket.io')(http);
 
-app.use(function(req, res, next){
-	res.set({
-		'Access-Control-Allow-Origin': '*'
-	});
-	next();
-});
+// app.use(function(req, res, next){
+// 	res.set({
+// 		'Access-Control-Allow-Origin': '*'
+// 	});
+// 	next();
+// });
 
 app.get('/', function(req, res){
-	res.send(404);
+	res.sendStatus(404);
 });
 
 io.on('connection', function(socket){
